683e256e33d16289fdcd22e87bc53b05bd5726e1
mae-zhangbj <1002129979@qq.com>
2025-06-21 18:09:59 +0800

    My_python_job/Usage_train.py update

--- RegDGCNN_SurfaceFields/My_python_job/Usage_train.py
+++ RegDGCNN_SurfaceFields/My_python_job/Usage_train.py
@@ -2,26 +2,30 @@
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
+
+# Import modules
+from model_pressure import RegDGCNN_pressure
 '''
+
 # DDP: Distributed Data Parallel
-1. 
+1.----
     world_size = len(gpu_list.split(','))
 #! 
     gpu_list                   -> "0, 1, 2"
     gpu_list.split(',')        -> ['0', '1', '2']
     len(gpu_list.split(','))   -> 3
 
-2.
+2.----
     exp_dir = os.path.join('experiments', args.exp_name)
 #!
     Build a path "./experiments/exp_name"
 
-3.
+3.----
     os.makedirs(exp_dir, exist_ok=True)
 #!
     Create the directory "exp_dir" if it doesn't already exist
 
-4. 
+4.----
     mp.spawn(train_and_evaluate, args=(world_size, args), nprocs=world_size, join=True)
 #!
    train_and_evaluate(rank, world_size, args)
@@ -37,18 +41,106 @@ import torch.multiprocessing as mp
     train_evaluate(rank=3, world_size= , args=args)
     ...
     
-5.
+5.----
     dist.init_process_group(backend='nccl', init_method='env://', world_size=world_size, rank=rank)
 #!
     Starts the communication backend for DDP
     # nccl:        NVDIA backend for multi-GPU
     # evv://       Uses evvironmen variables(e.g. RANK, WORLD_SIZE, etc)
 
-6. 
+6.-----
     torch.cuda.set_device(local_rank)
 #! 
     Each process uses a single GPU
     Set the GPU this process will use
+
+7.-----
+    args = vars(args)
+#!
+    Convert it to a regular dictionary
+    e.g. {'epoch': 10, 'lr': 0.0001}
+
+8.-----
+    model = RegDGCNN_pressure(args).to(local_rank)
+#!
+    RegDGCNN_pressure(args): Creates an instance of your custom model class
+    .to(local_rank):         Moves the model to the current corret GPU
+#!
+    The detail please see Usage_model_pressure.pu
+
+10.----
+    model = torch.nn.parallel.DistributedDataParallel(
+        model,
+        device_ids=[local_rank],
+        find_unused_parameters=True, 
+        output_device=local_rank
+    )
+#!
+    Wrap it with DDP so Pytorch handles multi-GPU
+        synchronization and data parallelism
+#!
+    model
+    The model aleady moved to the local GPU
+#!
+    device_ids=[local_rank]
+    Restrict this process to use only one GPU
+#!
+    find_unused_parameters=True
+    It tells pyTorch "Some layers in my model might not be used every time I call forward()
+    -> so please handle that correctly"
+        The detail please see Usage_model_pressure.py forwar() function
+
+#!  
+    output_device=local_rank
+    Ensures outputs go to the same GPU as inputs
+    
+11.----
+    criterion = torch.nn.MSELoss()
+#!
+    Loss function:
+    This sets up Mean Squared Error(MSE) as the loss function, commonly used for regression problems
+    It measures the average of the squares of the differences between predicted and actual values:
+        MSE = 1/n * sum((yi - y)^2)      i = 1, ... , n
+    
+12.----
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-4)
+#!
+    Uses Adam(Adaptive Moment Estimation), a popular optimizer that adjusts learning rates for each parameter
+#!
+    model.parameter()    
+    ->Tell the optimizer which parameters to update
+#!
+    lr = args.lr
+    ->learning rate 
+#!
+    weight_decay=1e-4
+    ->adds L2 regularization to reduce overfitting
+
+13.----
+    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=10, factor=0.1, verbose=True)
+#!    
+    Automatically reduces the learning rate when the validation loss plateaus(stops improving)
+#!
+    'min'
+    ->Try to minimize val loss
+    -> By default 1e-4
+#!
+    patience=10 
+    ->Wait for 10 epochs without improvement
+#!
+    factor=0.1
+    ->Multiplies LR by 0.1
+#!
+    e.g.
+    Epoch 1 - Validation Loss: 0.470
+    LR : 0.01
+    Epoch 2 - Validation Loss: 0.471
+    LR : 0.01
+    Epoch 3 - Validation Loss: 0.471
+    LR : 0.01
+    Epoch 4 - Validation Loss: 0.470
+    LR : 0.01*0.1
+    
 '''
 
 
