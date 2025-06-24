a00064e3b0be6480eb80306c43196e014685ef9d
mae-zhangbj <1002129979@qq.com>
2025-06-24 11:42:52 +0800

    My_python_job/Model_Training.sh update

--- RegDGCNN_SurfaceFields/My_python_job/Model_Training.sh
+++ RegDGCNN_SurfaceFields/My_python_job/Model_Training.sh
@@ -1,6 +1,6 @@
 python run_pipeline.py \
     --stages train \
-    --exp_name "Train Test" \
+    --exp_name "Train_Test" \
     --dataset_path "$HOME/ML_Turbulent/DrivAerNet/RegDGCNN_SurfaceFields/My_python_job/Pressure_VTK" \
     --subset_dir "$HOME/ML_Turbulent/DrivAerNet/train_val_test_splits" \
     --cache_dir "$HOME/ML_Turbulent/DrivAerNet/RegDGCNN_SurfaceFields/My_python_job/Cache_data" \
