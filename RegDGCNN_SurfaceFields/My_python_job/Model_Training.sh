dd601530f396e4334582da5ff0a4326e523cc2f5
mae-zhangbj <1002129979@qq.com>
2025-06-27 14:50:11 +0800

    Model_Training.sh update

--- RegDGCNN_SurfaceFields/My_python_job/Model_Training.sh
+++ RegDGCNN_SurfaceFields/My_python_job/Model_Training.sh
@@ -11,13 +11,3 @@ python run_pipeline.py \
     --gpus "0"
 
 
-# === Variable  ===
-# --batch_size 
-# It defines how many samples are processed at once per training step
-#
-# --epochs
-# 
-# Total number of training rounds over the whole dataset
-# e.g. 10000 samples, batch_size = 100
-#      10 000 / 100 = 100 steps for one epoch
-#      --epochs 150 have 150 times loop
