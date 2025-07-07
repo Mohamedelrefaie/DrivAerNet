0ed188c6c6a1d6a3a36cfa5900c8de8cee4b096b
mae-zhangbj <1002129979@qq.com>
2025-07-07 15:32:21 +0800

    My_python_job/Usage_python/Usage_Colorama.py update

---
+++ RegDGCNN_SurfaceFields/My_python_job/Usage_python/Usage_Colorama.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Style
+
+    logging.info(f"{Fore.GREEN}all_metrics: {all_metrics}{Style.RESET_ALL}")
+    -> {Style.RESET_ALL}
+       -> Reset the default color for the subsequent info
