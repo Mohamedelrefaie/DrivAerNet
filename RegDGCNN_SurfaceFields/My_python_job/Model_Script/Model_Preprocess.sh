e40066706c41c5f84da2588cb5a641edfc4b52af
mae-zhangbj <1002129979@qq.com>
2025-07-08 17:09:11 +0800

    Model_Script/Model_Preprocess.sh update

--- RegDGCNN_SurfaceFields/My_python_job/Model_Script/Model_Preprocess.sh
+++ RegDGCNN_SurfaceFields/My_python_job/Model_Script/Model_Preprocess.sh
@@ -1,10 +1,15 @@
-python ./run_pipeline.py \
+python run_pipeline.py \
     --stages preprocess \
-    --exp_name "DrivAerNet_Pressure" \
-    --dataset_path "$HOME/ML_Turbulent/Data_Pressure_Field/Data_Pressure/PressureVTK/N_S_WWS_WM" \
-    --cache_dir "$HOME/ML_Turbulent/Data_Pressure_Field/Cache_data" \
-    --subset_dir "$HOME/ML_Turbulent/Pressure_Field/train_val_test_splits" \
+    --exp_name "Train_Test" \
+    --dataset_path "$HOME/Data_Pressure/Pressure_VTK" \
+    --subset_dir "$HOME/ML_Turbulent/DrivAerNet/train_val_test_splits" \
+    --cache_dir "$HOME/Data_Pressure/Cache_data" \
     --num_points 10000
-    --batch_size 12 \
-    --epochs 150 \
-    --gpus "0, 1"
+
+#    --dataset_path "$HOME/Data_Pressure/Pressure_VTK" \
+#    --subset_dir "$HOME/ML_Turbulent/DrivAerNet/train_val_test_splits" \
+#    --cache_dir "$HOME/Data_Pressure/Cache_data" \
+
+#    --dataset_path "$HOME/ML_Turbulent/Data_Pressure_Field/Data_Pressure/PressureVTK/N_S_WWS_WM" \
+#    --subset_dir "$HOME/ML_Turbulent/DrivAerNet/train_val_test_splits" \
+#    --cache_dir "$HOME/ML_Turbulent/Data_Pressure_Field/Cache_data" \
