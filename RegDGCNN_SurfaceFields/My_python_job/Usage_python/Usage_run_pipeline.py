f51282520d7798e4768ba85228bac945a2f5605a
mae-zhangbj <1002129979@qq.com>
2025-07-08 17:11:14 +0800

    My_python_job/Usage_python/Usage_run_pipeline.py

--- RegDGCNN_SurfaceFields/My_python_job/Usage_python/Usage_run_pipeline.py
+++ RegDGCNN_SurfaceFields/My_python_job/Usage_python/Usage_run_pipeline.py
@@ -5,47 +5,53 @@
     -> Number of nearnest neighbors(for graph edge construction)
 
 2.
-    
+
 # ======== function usage ========
 
-1.
+#!-------------------------
     for key, value in env.items():
-        print(f"{key} = {value}") 
+        print(f"{key} = {value}")
 
 key                 : the name of environment variable
 value               : key's value
 f"{key} = {value}"  : f-strings(formatted strings)
 
-2. 
+#!-------------------------
     stages = args.stages.splits(',') if ',' in args.stages else [args.exp_name]
-#!
     This is a ternary expression
     stages = <A> if <condition> else <B>
-#!
-    e.g. --stage "preprocess, train"
-    -> stages = ['preprocess, train']
 
-    e.g. --stage "all"
-    -> stages = [all]
+    -> e.g. --stage "preprocess, train"
+       -> stages = ['preprocess, train']
 
-3. 
+    -> e.g. --stage "all"
+       -> stages = [all]
+
+#!-------------------------
     results = {}
-#!
-    create an empty directory
-#!
-    e.g. results = {
-        "preprocess": True, 
+    -> create an empty directory
+    -> e.g. results = {
+        "preprocess": True,
         "train":      flase,
         "evaluate":   True
     }
+    -> e.g. results.get('train', False)
+        -> resluts have train, get True
+           whether get False
 
-    e.g. results.get('train', False)
-    -> resluts have train, get True
-       whether get False
-4.
+#!-------------------------
     logging.info
-#!
-    Need explicit declaration
+    -> Need explicit declaration
 
 
+
+#!-------------------------
+def preprocess_data(args):
+    try:
+        # some code ...
+    except Exception as e:
+        logging.error(f"Preprocessing failed with error: {e}")
+        return False
+    -> If any error happens in the try block, Python immediately jumps to except block instead of crashing
+    -> The error object is saved as e
 
