485feb489eddc28e7ccc08f673201c61748d1c43
mae-zhangbj <1002129979@qq.com>
2025-07-09 15:30:39 +0800

    My_python_job/Model_Script/Model_Preprocess.sh update

--- RegDGCNN_SurfaceFields/My_python_job/Model_Script/Model_Preprocess.sh
+++ RegDGCNN_SurfaceFields/My_python_job/Model_Script/Model_Preprocess.sh
@@ -1,10 +1,10 @@
 python run_pipeline.py \
     --stages preprocess \
     --exp_name "Train_Test" \
-    --dataset_path "$HOME/Data_Pressure/Pressure_VTK" \
+    --dataset_path "$HOME/ML_Turbulent/Data_Pressure_Field/Data_Pressure/PressureVTK/N_S_WWS_WM" \
     --subset_dir "$HOME/ML_Turbulent/DrivAerNet/train_val_test_splits" \
-    --cache_dir "$HOME/Data_Pressure/Cache_data" \
-    --num_points 10000
+    --cache_dir "$HOME/ML_Turbulent/Data_Pressure_Field/Cache_data" \
+    --num_points 100000
 
 #    --dataset_path "$HOME/Data_Pressure/Pressure_VTK" \
 #    --subset_dir "$HOME/ML_Turbulent/DrivAerNet/train_val_test_splits" \
