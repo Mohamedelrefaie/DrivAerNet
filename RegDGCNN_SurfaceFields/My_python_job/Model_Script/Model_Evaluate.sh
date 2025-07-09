d8b400d4a8b668ab0a95b095d5282ef37957e975
mae-zhangbj <1002129979@qq.com>
2025-07-09 15:30:21 +0800

    My_python_job/Model_Script/Model_Evaluate.sh update

--- RegDGCNN_SurfaceFields/My_python_job/Model_Script/Model_Evaluate.sh
+++ RegDGCNN_SurfaceFields/My_python_job/Model_Script/Model_Evaluate.sh
@@ -4,6 +4,6 @@ python ./run_pipeline.py \
     --dataset_path "$HOME/ML_Turbulent/Data_Pressure_Field/Data_Pressure/PressureVTK/N_S_WWS_WM" \
     --subset_dir "$HOME/ML_Turbulent/DrivAerNet/train_val_test_splits" \
     --cache_dir "$HOME/ML_Turbulent/Data_Pressure_Field/Cache_data" \
-    --num_points 10000 \
+    --num_points 100000 \
     --num_eval_samples 5 \
     --gpus "0"
